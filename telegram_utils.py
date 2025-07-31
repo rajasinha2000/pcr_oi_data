4940dafc53b924c34739caee6024b12cd7742124
rajasinha2000 <mdrinfotech79@gmail.com>
2025-07-31 16:46:06 +0530

    Create telegram_utils.py

---
+++ telegram_utils.py
@@ -0,0 +1,17 @@
+# telegram_utils.py
+import requests
+from config import BOT_TOKEN, CHAT_ID
+
+def send_telegram_alert(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+    try:
+        response = requests.post(url, json=payload)
+        return response.status_code == 200
+    except Exception as e:
+        print("Telegram Error:", e)
+        return False
